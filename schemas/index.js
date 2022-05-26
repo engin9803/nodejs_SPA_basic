b9fd180fcbd095d4796b4217bba0ae8f5b345a45
engin9803 <mne9691@gmail.com>
2022-05-26 14:51:35 +0900

    몽고DB localhost로 다시 변경

--- schemas/index.js
+++ schemas/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-    mongoose.connect("mongodb://3.39.226.20:27017/SPA_HOMEWORK", { ignoreUndefined: true }).catch((err) => {
+    mongoose.connect("mongodb://localhost:27017/SPA_HOMEWORK", { ignoreUndefined: true }).catch((err) => {
         console.error(err);
     });
 };
